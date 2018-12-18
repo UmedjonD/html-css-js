8039381b821f917197ad0fce8f38dbb69d7a6431
UmedjonD <lazerr_94@mail.ru>
2018-12-18 19:03:28 +0400

    блоки энд онклик

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById('vhod');
+    alert("не так быстро");
+}
+function myFunction2() {
+    document.getElementById('registration');
+    alert("да конечно!");
+}
+function myFunction3() {
+    document.getElementById('vhod1');
+    alert("не смотри так!");
+}
+function product1(Element){
+    // document.getElementById('21');
+    alert(Element);
+    
+}
+
