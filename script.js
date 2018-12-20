17b0d8b16f5ed18e3ca8138eb2c0daa6d038c60e
Umedjon <Lazerr_94@mail.ru>
2018-12-20 23:55:00 +0300

    pizdesBlyatNahoi

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-function myFunction() {
-    document.getElementById('vhod');
-    alert("не так быстро");
-}
-function myFunction2() {
-    document.getElementById('registration');
-    alert("да конечно!");
-}
-function myFunction3() {
-    document.getElementById('vhod1');
-    alert("не смотри так!");
-}
-function product1(Element){
-    // document.getElementById('21');
-    //alert(Element);
-    Element.style.cssText = "width: 100%";
+$('.product').on('click', function() {
+    $('.product').show();
+    $('.product_detail').hide();
+    var preview = $(this);
+    var detail = $(this).next();
+    preview.hide();
+    detail.show();
+    preview.detach().appendTo('#viewed');
+    detail.detach().appendTo('#viewed');
+});
+
+$('.product_detail .add').on('click', function() {
+    var detail = $(this).parents('.product_detail');
+    var name = detail.find('.name').text();
+    var price = detail.find('.price').text();
+    var priceInt = parseInt(price);
+    var img = detail.find('img').attr('src');
     
-}
+    $('#count').text(parseInt($('#count').text()) + 1);
 
+    var summa = parseInt($('#summa').text()) + priceInt;
+    var skidka = Math.ceil(summa * 0.1)
+    var itogo = summa - skidka;
 
+    $('#summa').text(summa);
+    $('#skidka').text(skidka);
+    $('#itogo').text(itogo);
+
+    $('#okno_elements').append(
+        '<div>' +
+            '<div> <img src="' + img + '"></div>' +
+            '<div class="nameK">'  + name + '</div>' +
+            '<div>'  + price + '</div>' +
+        '</div>'
+    );
+});
