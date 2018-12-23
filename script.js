ac555feb78c8979b4313cf9620af74fd26d7e23f
Umedjon <Lazerr_94@mail.ru>
2018-12-23 23:40:24 +0300

    изменение

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 $(function() {
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
     $("form[name='registration']").validate({
-      // Specify validation rules
+      // правила проверки
       rules: {
-        // The key name on the left side is the name attribute
-        // of an input field. Validation rules are defined
-        // on the right side
         firstname: "required",
         lastname: "required",
         email: {
           required: true,
-          // Specify that email should be validated
-          // by the built-in "email" rule
           email: true
         },
         password, password2: {
@@ -20,18 +13,16 @@ $(function() {
           minlength: 5
         }
       },
-      // Specify validation error messages
+      // мессеж при ошибки
       messages: {
-        firstname: "Please enter your firstname",
-        lastname: "Please enter your lastname",
+        firstname: "Пожалуйста введите имя",
+        lastname: "пожайлуйста введите фамилию",
         password: {
-          required: "Please provide a password",
-          minlength: "Your password must be at least 5 characters long"
+          required: "введите пароль",
+          minlength: "пароль не должен быть менее 5 символов"
         },
-        email: "Please enter a valid email address"
+        email: "введите емайл"
       },
-      // Make sure the form is submitted to the destination defined
-      // in the "action" attribute of the form when valid
       submitHandler: function(form) {
         form.submit();
       }
@@ -41,8 +32,8 @@ $(function() {
 $('.product').on('click', function() {
     $('.product').show();
     $('.product_detail').hide();
-    var preview = $(this);
-    var detail = $(this).next();
+    let preview = $(this);
+    let detail = $(this).next();
     preview.hide();
     detail.show();
     preview.detach().appendTo('#viewed');
@@ -50,27 +41,27 @@ $('.product').on('click', function() {
 });
 
 $('.product_detail .add').on('click', function() {
-    var detail = $(this).parents('.product_detail');
-    var name = detail.find('.name').text();
-    var price = detail.find('.price').text();
-    var priceInt = parseInt(price);
-    var img = detail.find('img').attr('src');
+    let detail = $(this).parents('.product_detail');
+    let name = detail.find('.name').text();
+    let price = detail.find('.price').text();
+    let priceInt = parseInt(price);
+    let img = detail.find('img').attr('src');
     
     $('#count').text(parseInt($('#count').text()) + 1);
 
-    var summa = parseInt($('#summa').text()) + priceInt;
-    var skidka = Math.ceil(summa * 0.1)
-    var itogo = summa - skidka;
+    let summa = parseInt($('#summa').text()) + priceInt;
+    let skidka = Math.ceil(summa * 0.1)
+    let itogo = summa - skidka;
 
     $('#summa').text(summa);
     $('#skidka').text(skidka);
     $('#itogo').text(itogo);
 
     $('#okno_elements').append(
-        '<div>' +
+        '<div class="in_korzina">' +
             '<div> <img src="' + img + '"></div>' +
             '<div class="nameK">'  + name + '</div>' +
-            '<div>'  + price + '</div>' +
+            '<div class="priceK">'  + price + '</div>' +
         '</div>'
     );
 });
