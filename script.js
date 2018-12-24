65283e11daa702468fc77802b298087b9e809907
Umedjon <Lazerr_94@mail.ru>
2018-12-25 01:27:16 +0300

    добавил волидацию , вроде теперь ок

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-// $(function() {
-//     $("form[name='registration']").validate({
-//       // правила проверки
-//       rules: {
-//         firstname: "required",
-//         lastname: "required",
-//         email: {
-//           required: true,
-//           email: true
-//         },
-//         password, password2: {
-//           required: true,
-//           minlength: 5
-//         }
-//       },
-//       // мессеж при ошибки
-//       messages: {
-//         firstname: "Пожалуйста введите имя",
-//         lastname: "пожайлуйста введите фамилию",
-//         password: {
-//           required: "введите пароль",
-//           minlength: "пароль не должен быть менее 5 символов"
-//         },
-//         email: "введите емайл"
-//       },
-//       submitHandler: function(form) {
-//         form.submit();
-//       }
-//     });
-//   });
+$(function() {
+    $("form[name='registration']").validate({
+      // правила проверки
+      rules: {
+        firstname: "required",
+        lastname: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 5
+        },
+        password2: {
+            required: true,
+            minlength: 5
+          }
+      },
+      // мессеж при ошибки
+      messages: {
+        firstname: "пожалуйста введите имя",
+        lastname: "пожалуйста введите фамилию",
+        password: {
+          required: "введите пароль",
+          minlength: "пароль не должен быть менее 5 символов"
+        },
+        password2: {
+            required: "повторите пароль",
+            minlength: "пароль не должен быть менее 5 символов"  
+        },
+        email: "введите емайл"
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
 
 $('.product').on('click', function() {
     $('.product').show();
@@ -57,11 +65,24 @@ $('.product_detail .add').on('click', function() {
     $('#skidka').text(skidka);
     $('#itogo').text(itogo);
 
-    $('#okno_elements').append(
-        '<div class="in_korzina">' +
-            '<div> <img src="' + img + '"></div>' +
-            '<div class="nameK">'  + name + '</div>' +
-            '<div class="priceK">'  + price + '</div>' +
-        '</div>'
-    );
+   var elementHtml = $('<tr>' +
+        '<td><img src="' + img + '"></td>' +
+        '<td>' + name + '</td>' +
+        '<td>' + price + '</td>' +
+        '<td><span class="close">x</span></td>' +
+   '</tr>');
+
+   elementHtml.find('.close').on('click', function() {
+       elementHtml.remove();
+       $('#count').text(parseInt($('#count').text()) - 1);
+        let summa = parseInt($('#summa').text()) - priceInt;
+        let skidka = Math.ceil(summa * 0.1)
+        let itogo = summa - skidka;
+        
+        $('#summa').text(summa);
+        $('#skidka').text(skidka);
+        $('#itogo').text(itogo);
+   });
+
+    $('#okno_elements_table').append(elementHtml);
 });
